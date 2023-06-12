862f290c77c20947110338d4efd9d9136b9056e7
Lily Hopkins <lily@hpkns.uk>
2023-06-12 12:21:07 +0100

    docs: added README

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use regex::Regex;
 /// Highlight parts of a file given a syntax.
 ///
 /// We parse over a syntax CSV, expecting a header row containing `start,length,name,condition', where:
-///   `start` is the 1-based start column of the character to hightlight
+///   `start` is the 1-based start column of the character to highlight
 ///   `length` is the number of columns of this field
 ///   `name` is the human readable name for this field
 ///   `condition` (optional) is a regex to restrict this rule applying except to lines that match the regex.
